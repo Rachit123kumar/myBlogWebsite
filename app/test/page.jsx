216d65d42d447cff8e69b961c2e5273fa4e975c6
Rachit123kumar <hellobittukumar12@gmail.com>
2025-06-23 12:38:22 +0530

    adding google seo

---
+++ app/test/page.jsx
@@ -0,0 +1,33 @@
+
+// 'use client'
+
+// import { useEffect } from "react"
+
+export default function page() {
+
+// curl '' --header 'Authorization: Bearer YOUR_BEARER_TOKEN'
+
+    // useEffect(() => {
+    // async function getTwitterTrends() {
+    //         const res = await fetch("https://api.x.com/2/trends/by/woeid/23424775", {
+    //             headers: {
+    //                 Authorization: `AAAAAAAAAAAAAAAAAAAAACEcsgEAAAAAkm133%2BMV5lTriBVtPIrfM7%2BWSPE%3DJ9I1auEOBjbUOtWBTN9xtC340Yl6VzaSQFnq3zhOdd8PdB7l0N`,
+    //             },
+    //         });
+
+    //         const data = await res.json();
+    //         console.log(data)
+    //         return data[0].trends.slice(0, 10); // Top 10 trends
+    //     }
+    //     getTwitterTrends()
+
+    // }, [])
+
+
+
+
+
+    return (
+        <div>page of Testing</div>
+    )
+}
